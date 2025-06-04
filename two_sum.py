dddc27caccaf294e8f407069c26831fdd600f799
Sabrina Bilmak <bilmak.sabrina@gmail.com>
2025-06-04 15:39:24 +0200

    linkedlist

--- two_sum.py
+++ two_sum.py
@@ -27,3 +27,9 @@ def two(nums: list[int], target:int)-> list[int]:
     return []
 
 
+
+def summm(nums:list, target):
+    for k, v in nums:
+        for i, y in nums:
+            if v+y==target:
+                return k, i
